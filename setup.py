16b8af7ea5444536776d0f0d014020a3f4f62af1
Eric Pascual <eric@pobot.org>
2014-04-05 13:30:10 +0200

    changed package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='PJC_tournament_manager',
+    name='PJC_competition_management',
     version='1.0',
     packages=find_packages(),
     include_package_data=True,
