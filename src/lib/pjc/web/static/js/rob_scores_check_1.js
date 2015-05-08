d1234a6679214356ad6f1b1a5d77753da0be1e6a
Eric Pascual <eric@pobot.org>
2015-05-08 23:59:55 +0200

    updated for 2015 rules

--- src/lib/pjc/web/static/js/rob_scores_check_1.js
+++ src/lib/pjc/web/static/js/rob_scores_check_1.js
@@ -1,12 +1,10 @@
 function check_row(team_num, row) {
+    "use strict";
     var total_time = $("#total_time_" + team_num).val();
-    var alignments = $("#alignments_" + team_num).val();
-    var dockings = $("#dockings_" + team_num).val();
-    var hits = $("#hits_" + team_num).val();
+    var sections = parseInt($("#sections_" + team_num).val(), 10);
 
-    if ((total_time == '00:00') &&
-        (alignments != 0 || dockings != 0 || hits != 0)) {
-       return "Temps total manquant"
+    if ((total_time === '00:00') && (sections !== 0)) {
+        return "Temps total manquant";
     }
 
     return null;
